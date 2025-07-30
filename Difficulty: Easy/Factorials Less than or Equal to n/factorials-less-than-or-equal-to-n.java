2d9059a7f812ef2ca279bf7e840c7a1e2dde3e7c
Yash Zape <yashzape8@gmail.com>
2025-07-30 22:24:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Factorials Less than or Equal to n/factorials-less-than-or-equal-to-n.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+class Solution {
+    static ArrayList<Long> factorialNumbers(long n) {
+        // code here
+     ArrayList<Long> result = new ArrayList<>();
+     long fact = 1;
+     int i =1;
+     
+     while(fact<=n){
+         result.add(fact);
+         i++;
+         fact = fact * i;
+     }
+     
+     return result;
+    
+    }
+}
