0b80aad9d47bbd832f91e858ef14d8aea15bdec9
Yash Zape <yashzape8@gmail.com>
2025-07-30 22:20:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GFG n times/print-gfg-n-times.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    void printGfg(int N) {
+        // code here
+        if(N==0){
+            return;
+        }
+        printGfg(N-1);
+        System.out.print("GFG ");
+    }
+}
